998e80a318e3c819bd1496fe22f9297587865202
Leman-Quinn <PabloN.A@hotmail.com>
2025-05-30 20:37:09 -0300

    Se agrego clase NegocioProvincia

--- Datos/DaoProvincia.cs
+++ Datos/DaoProvincia.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Datos
 {
-    internal class Class1
+    public  class DaoProvincia
     {
+        AccesoDatos accesoDatos = new AccesoDatos();
+        public DataTable ObtenerProvincias()
+        {
+            string consultaSQL = "Select * FROM Provincia";
+            return accesoDatos.ObtenerTablas(consultaSQL, "Provincias");
+        }
+
     }
 }
--- Entidades/Provincia.cs
+++ Entidades/Provincia.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    internal class Class1
+    public class Provincia
     {
+        public int _Id_Provincia;
+        public string _DescripcionProvincia;
+
+        public Provincia()
+        {
+
+        }
+        public int getId_Provincia()
+        {
+            return _Id_Provincia;
+        }
+        public void setId_Provincia(int idProvincia)
+        {
+            _Id_Provincia = idProvincia;
+        }
+
+        public string getDescripcionProvincia()
+        {
+            return _DescripcionProvincia;
+        }
+        public void setDescripcionProvincia(string DescripcionProvincia)
+        {
+            _DescripcionProvincia = DescripcionProvincia;
+        }
     }
 }
